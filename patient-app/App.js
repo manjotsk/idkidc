13d7957bd30ce06acbcad52224cfad6986a4722c
Lavish Thakkar <lavisht22@gmail.com>
2018-10-28 00:15:28 +0530

    Patient App Initialization

--- patient-app/App.js
+++ patient-app/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, Easing } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import HomeScreen from './views/home';
+import TrackingScreen from './views/tracking';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const transitionConfig = () => ({
+  transitionSpec: {
+    duration: 500,
+    easing: Easing.out(Easing.poly(6)),
+    timing: Animated.timing,
+    useNativeDriver: true,
+  },
+  screenInterpolator: sceneProps => {
+    const { position, layout, scene } = sceneProps;
+
+    const thisSceneIndex = scene.index;
+    const height = layout.initHeight;
+
+    const translateY = position.interpolate({
+      inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+      outputRange: [height, 0, 0],
+    });
+
+    const slideFromBottom = { transform: [{ translateY }] };
+
+    return slideFromBottom;
   },
 });
+
+const RootStack = createStackNavigator(
+  {
+    HomeScreen,
+    TrackingScreen,
+  },
+  {
+    initialRouteName: 'HomeScreen',
+    transitionConfig,
+    lazy: true,
+    headerMode: 'none',
+    cardStyle: {
+      shadowColor: 'transparent',
+    },
+  }
+);
+
+export default RootStack;
---
+++ patient-app/views/tracking/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './style';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
