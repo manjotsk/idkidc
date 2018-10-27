841857dc28d8baa85a9775dd92eb0158e4c5ed6e
manjot singh <manjot.kalsi@simbaquartz.com>
2018-10-28 01:44:54 +0530

    Integrated mapView with sample coordinated

--- patient-app/views/tracking/index.js
+++ patient-app/views/tracking/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-import styles from './style';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {[
+          {
+            latlng: {
+              latitude: 37.78825,
+              longitude: -122.4324,
+            },
+            title: 'Ambulance 1',
+            description: 'I am available',
+          },
+        ].map((marker, key) => (
+          <Marker
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+            key={key}
+          />
+        ))}
+      </MapView>
     );
   }
 }
