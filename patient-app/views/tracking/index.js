0e1f9a41f53f22a6a4c1d5181474eaf0584f5c8b
Lavish Thakkar <lavisht22@gmail.com>
2018-10-28 02:48:31 +0530

    Tracking screen (#3)
    * Integrated mapView with sample coordinated
    
    * updated git ignore
    
    * changed icon for pin

--- patient-app/views/tracking/index.js
+++ patient-app/views/tracking/index.js
@@ -1,14 +1,75 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { TouchableOpacity, View, Text } from 'react-native';
+import ambulanceIcon from '../../assets/img/ambulance.png';
 
-import styles from './style';
+const sampleCoordinates= [
+  {
+    latlng: {
+      latitude: 24.78825,
+      longitude: -12.4324,
+    },
+    title: 'Ambulance 1',
+    description: 'I am available',
+  },
+  {
+    latlng: {
+      latitude: -37.78825,
+      longitude: -22.4324,
+    },
+    title: 'Ambulance 2',
+    description: 'I am available',
+  },
+  {
+    latlng: {
+      latitude: 37.78825,
+      longitude: -12.4324,
+    },
+    title: 'Ambulance 1',
+    description: 'I am available',
+  },
+];
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {[
+          {
+            latlng: {
+              latitude: 37.78825,
+              longitude: -122.4324,
+            },
+            title: 'Ambulance 1',
+            description: 'I am available',
+          },
+        ].map((marker, key) => (
+          <Marker
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+            key={key}
+            image={ambulanceIcon}
+          >
+            <Callout tooltip>
+              <TouchableOpacity onPress={() => alert(`Clicked`)}>
+                <View style={{backgroundColor:'#FFFFFF'}}>
+                  <Text>{marker.title}{"\n"}{marker.description}</Text>
+                </View>
+              </TouchableOpacity>
+            </Callout>
+
+          </Marker>
+        ))}
+      </MapView>
     );
   }
 }
